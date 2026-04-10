8aaf6946f900a673c03df5c41d37ba137833f0f8
mozammal-hossain <mozammal.hossain@brainstation-23.com>
2026-04-10 11:58:27 +0600

    Refactor server setup to use chi router and add middleware; create new router package for better organization

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,26 +1,17 @@
-package main
+package router
 
 import (
-	"log"
-	"net/http"
-	"os"
-
 	"example.com/hostel-management/internal/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
+func New() *chi.Mux {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
 	r.Get("/health", handlers.Health)
 
-	log.Printf("listening on :%s", port)
-	log.Fatal(http.ListenAndServe(":"+port, r))
+	return r
 }
